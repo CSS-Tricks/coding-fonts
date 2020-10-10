bc51d52454a187d1255ff98ada6adb0576e1f6e1
Chris Coyier <chriscoyier@gmail.com>
2020-10-10 15:32:28 -0700

    simplify injection to function

--- samples/main.js
+++ samples/main.js
@@ -1,4 +1,15 @@
 (function () {
+  function injectStylesheet(url) {
+    const head = document.getElementsByTagName("head")[0];
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = url;
+    link.media = "all";
+    head.appendChild(link);
+  }
+
+  // Get info from URL on how to set up page.
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
 
@@ -9,26 +20,14 @@
   } else {
     stylesheet = "/lib/prism-light.css";
   }
-  const head = document.getElementsByTagName("head")[0];
-  const link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.type = "text/css";
-  link.href = stylesheet;
-  link.media = "all";
-  head.appendChild(link);
+  injectStylesheet(stylesheet);
 
   // Set the font
   const font = urlParams.get("font");
   const code = document.querySelector("pre > code");
 
   if (font === "Fira Code") {
-    const head = document.getElementsByTagName("head")[0];
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "/fonts/Fira_Code_v5.2/fira_code.css";
-    link.media = "all";
-    head.appendChild(link);
+    injectStylesheet("/fonts/Fira_Code_v5.2/fira_code.css");
     code.style.fontFamily = "Fira Code";
   } else if (font === "Operator Mono") {
     // Not free/open source, so assumes is locally active
