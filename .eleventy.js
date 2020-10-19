4fa6e6aa3cad048ff1afecc5266b2cfe80b5063e
Chris Coyier <chriscoyier@gmail.com>
2020-10-19 10:40:29 -0700

    add fonts for production

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,23 @@
 module.exports = function (config) {
-  config.addPassthroughCopy("src/assets");
-  config.addPassthroughCopy("src/screenshots");
+  config.addPassthroughCopy('src/assets');
+  config.addPassthroughCopy('src/screenshots');
+  config.addPassthroughCopy('src/fonts-for-ui');
 
-  config.addCollection("fonts", function (collection) {
-    let fonts = collection.getFilteredByGlob("src/fonts/*.md"); 
+  config.addCollection('fonts', function (collection) {
+    let fonts = collection.getFilteredByGlob('src/fonts/*.md');
     return fonts;
   });
 
   return {
     dir: {
-      input: "src",
-      output: "dist",
-      includes: "_includes",
-      data: "_data",
+      input: 'src',
+      output: 'dist',
+      includes: '_includes',
+      data: '_data'
     },
     passthroughFileCopy: true,
-    templateFormats: ["njk", "md"],
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
+    templateFormats: ['njk', 'md'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk'
   };
 };
