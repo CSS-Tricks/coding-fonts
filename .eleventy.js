5d681e22d0542fba409124daf7920b681da577fb
Chris Coyier <chriscoyier@gmail.com>
2020-11-11 06:37:41 -0800

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
 module.exports = function (config) {
   config.addPassthroughCopy('src/assets');
+  config.addPassthroughCopy('src/favicon.ico');
   config.addPassthroughCopy('src/screenshots');
   config.addPassthroughCopy('src/fonts-for-ui');
 
   config.addCollection('fonts', function (collection) {
     let fonts = collection
-        .getFilteredByGlob('src/fonts/*.md')
-        .sort((a, b) => b.data.title < a.data.title ? 1 : -1);
+      .getFilteredByGlob('src/fonts/*.md')
+      .sort((a, b) => (b.data.title < a.data.title ? 1 : -1));
     return fonts;
   });
 
