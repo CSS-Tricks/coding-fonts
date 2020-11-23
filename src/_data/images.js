4583b57da4ce4e574c15dd49a512e472775e9259
Jonathan Land <land.jonathan@gmail.com>
2020-11-23 19:08:06 +0200

    adding check for Netlify deploy preview to images

--- src/_data/images.js
+++ src/_data/images.js
@@ -1,16 +1,19 @@
+const isDeployPreview = process.env.CONTEXT === 'deploy-preview'
+const isProd = process.env.PROD === 'true' && !isDeployPreview
+
 module.exports = {
-  imageLocation:
-    process.env.PROD === 'true' ? 'https://coding-fonts.css-tricks.com' : '',
-  urlPrefixLarge:
-    process.env.PROD === 'true'
+  imageLocation: 
+    isProd ? 'https://coding-fonts.css-tricks.com' : '',
+  urlPrefixLarge: 
+    isProd
       ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_1600,q_auto,f_auto/'
       : '',
   urlPrefixMedium:
-    process.env.PROD === 'true'
+    isProd
       ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_1200,q_auto,f_auto/'
       : '',
   urlPrefixSmall:
-    process.env.PROD === 'true'
+    isProd
       ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_600,q_auto,f_auto/'
       : ''
 };
