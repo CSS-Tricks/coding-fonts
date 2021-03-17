c87b9ffd3780375f5c7e01c6cea3241cd7509232
Chris Coyier <chriscoyier@gmail.com>
2021-03-17 14:41:59 -0700

    trying to not use f_auto & q_auto

--- src/_data/images.js
+++ src/_data/images.js
@@ -1,19 +1,15 @@
-const isDeployPreview = process.env.CONTEXT === 'deploy-preview'
-const isProd = process.env.PROD === 'true' && !isDeployPreview
+const isDeployPreview = process.env.CONTEXT === 'deploy-preview';
+const isProd = process.env.PROD === 'true' && !isDeployPreview;
 
 module.exports = {
-  imageLocation: 
-    isProd ? 'https://coding-fonts.css-tricks.com' : '',
-  urlPrefixLarge: 
-    isProd
-      ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_1600,q_auto,f_auto/'
-      : '',
-  urlPrefixMedium:
-    isProd
-      ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_1200,q_auto,f_auto/'
-      : '',
-  urlPrefixSmall:
-    isProd
-      ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_600,q_auto,f_auto/'
-      : ''
+  imageLocation: isProd ? 'https://coding-fonts.css-tricks.com' : '',
+  urlPrefixLarge: isProd
+    ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_1600/'
+    : '',
+  urlPrefixMedium: isProd
+    ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_1200/'
+    : '',
+  urlPrefixSmall: isProd
+    ? 'https://res.cloudinary.com/css-tricks/image/fetch/w_600/'
+    : ''
 };
